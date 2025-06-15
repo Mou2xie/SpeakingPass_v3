996549e4ecbdbdd826d5c219c3a3a7c58c52329e
Yongjie Xie 谢师傅 <xieyongjie2012@163.com>
2025-06-14 20:43:55 -0400

    feat: Update title for IELTS Speaking questions section; add new actions and supabase files

--- src/app/[lan]/page.tsx
+++ src/app/[lan]/page.tsx
@@ -44,7 +44,7 @@ export default async function Page({ params }: { params: Promise<{ lan: string }
                 <p className=" text-[1.0rem] lg:text-[1.4rem] text-center font-(family-name:--font-ptSerif) text-text-main">Get access to all past and latest IELTS Speaking questions to ace your test!</p>
                 <Image src='/heroimage.svg' alt="hero-image" width={280} height={30} className=" mt-5 lg:mt-10 lg:w-[500px]"></Image>
             </section>
-            <Title>Questions for May-September</Title>
+            <Title>Questions for May-August</Title>
             <PartOneSection topics1={topics1} />
             <PartTwoSection topics2={topics2} />
         </>
